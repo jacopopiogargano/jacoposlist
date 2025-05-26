6487d25d7da8cac69d5183df45a7eb00e9766253
Jacopo <j.gargano@infos.it>
2025-05-26 04:48:42 -0700

    Fixed img path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
   return (
     <div className="flex items-center justify-center min-h-[calc(100vh-4rem)]">
       <Image
-        src="/img/logo.jpg"
+        src="./img/logo.jpg"
         alt="Guernica"
         width={1200}
         height={600}
